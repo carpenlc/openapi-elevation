111b1d910f449fc5862a80b4b99d2611d3d97af8
carpenlc <carpenlc@ndwdsndwlpgrd01.arn.gov>
2019-08-29 15:57:35 +0000

    Added a couple of unit test cases.

---
+++ src/test/java/mil/nga/elevation/utils/TestConversionUtils.java
@@ -0,0 +1,113 @@
+package mil.nga.elevation.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import mil.nga.elevation.ErrorCodes;
+import mil.nga.elevation.exceptions.ApplicationException;
+import mil.nga.elevation.model.GeodeticCoordinate;
+import mil.nga.elevation_services.model.HeightUnitType;
+import mil.nga.elevation_services.model.TerrainDataFileType;
+
+public class TestConversionUtils {
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	@Test
+	public void testConvertHeightUnitType() throws ApplicationException {
+		Assert.assertEquals(HeightUnitType.FEET, ConversionUtils.convertHeightUnitType("FEET"));
+		Assert.assertEquals(HeightUnitType.FEET, ConversionUtils.convertHeightUnitType("feet"));
+		Assert.assertEquals(HeightUnitType.METERS, ConversionUtils.convertHeightUnitType("METERS"));
+		Assert.assertEquals(HeightUnitType.METERS, ConversionUtils.convertHeightUnitType("meters"));
+		Assert.assertEquals(HeightUnitType.METERS, ConversionUtils.convertHeightUnitType(null));
+	}
+	
+	@Test
+	public void testConvertHeightUnitTypeFailure() throws ApplicationException {
+		thrown.expect(ApplicationException.class);
+		thrown.expectMessage(ErrorCodes.INVALID_UNITS.getErrorMessage());
+		ConversionUtils.convertHeightUnitType("BLAH");
+	}
+	
+	@Test
+	public void testConvertTerrainDataFileType() throws ApplicationException {
+		Assert.assertEquals(TerrainDataFileType.DTED0, ConversionUtils.convertTerrainDataFileType(null));
+		Assert.assertEquals(TerrainDataFileType.DTED0, ConversionUtils.convertTerrainDataFileType(""));
+		Assert.assertEquals(TerrainDataFileType.DTED0, ConversionUtils.convertTerrainDataFileType("DTED0"));
+		Assert.assertEquals(TerrainDataFileType.DTED0, ConversionUtils.convertTerrainDataFileType("dted0"));
+		Assert.assertEquals(TerrainDataFileType.DTED1, ConversionUtils.convertTerrainDataFileType("DTED1"));
+		Assert.assertEquals(TerrainDataFileType.DTED1, ConversionUtils.convertTerrainDataFileType("dted1"));
+		Assert.assertEquals(TerrainDataFileType.DTED2, ConversionUtils.convertTerrainDataFileType("DTED2"));
+		Assert.assertEquals(TerrainDataFileType.DTED2, ConversionUtils.convertTerrainDataFileType("dted2"));
+		Assert.assertEquals(TerrainDataFileType.BEST, ConversionUtils.convertTerrainDataFileType("BEST"));
+		Assert.assertEquals(TerrainDataFileType.BEST, ConversionUtils.convertTerrainDataFileType("best"));
+		Assert.assertEquals(TerrainDataFileType.SRTM1, ConversionUtils.convertTerrainDataFileType("SRTM1"));
+		Assert.assertEquals(TerrainDataFileType.SRTM1, ConversionUtils.convertTerrainDataFileType("srtm1"));
+		Assert.assertEquals(TerrainDataFileType.SRTM2, ConversionUtils.convertTerrainDataFileType("SRTM2"));
+		Assert.assertEquals(TerrainDataFileType.SRTM2, ConversionUtils.convertTerrainDataFileType("srtm2"));
+		Assert.assertEquals(TerrainDataFileType.SRTM1F, ConversionUtils.convertTerrainDataFileType("SRTM1F"));
+		Assert.assertEquals(TerrainDataFileType.SRTM1F, ConversionUtils.convertTerrainDataFileType("srtm1f"));
+		Assert.assertEquals(TerrainDataFileType.SRTM2F, ConversionUtils.convertTerrainDataFileType("SRTM2F"));
+		Assert.assertEquals(TerrainDataFileType.SRTM2F, ConversionUtils.convertTerrainDataFileType("srtm2f"));
+	}
+	
+	@Test
+	public void testConvertTerrainDataFileTypeFailure() throws ApplicationException {
+		thrown.expect(ApplicationException.class);
+		thrown.expectMessage(ErrorCodes.INVALID_SOURCE_TYPE.getErrorMessage());
+		ConversionUtils.convertTerrainDataFileType("BLAHBLAH");
+	}
+	
+	@Test
+	public void testParseInputCoordinateList() throws ApplicationException {
+		List<String> pts = new ArrayList<String>(); 
+		pts.add("106 23 38 W");
+		pts.add("37 20 19 N");
+		pts.add("9.3");
+		pts.add("9.3");
+		pts.add("106 20 00 W");
+		pts.add("37 20 00 N");
+		List<GeodeticCoordinate> parsed = ConversionUtils.parseInputCoordinateList(pts);
+		Assert.assertEquals(3, parsed.size());
+	}
+	
+	@Test
+	public void testParseInputCoordinateListFailure1() throws ApplicationException {
+		thrown.expect(ApplicationException.class);
+		thrown.expectMessage(ErrorCodes.INVALID_NUMBER_OF_INPUT_COORDINATES.getErrorMessage());
+		List<String> pts = new ArrayList<String>(); 
+		pts.add("106 23 38 W");
+		pts.add("37 20 19 N");
+		pts.add("9.3");
+		pts.add("9.3");
+		pts.add("106 20 00 W");
+		ConversionUtils.parseInputCoordinateList(pts);
+	}
+	
+	@Test
+	public void testParseInputCoordinateListFailure2() throws ApplicationException {
+		thrown.expect(ApplicationException.class);
+		thrown.expectMessage(ErrorCodes.INVALID_INPUT_COORDINATES.getErrorMessage());
+		List<String> pts = new ArrayList<String>(); 
+		ConversionUtils.parseInputCoordinateList(pts);
+	}
+	
+	@Test
+	public void testToString1() {
+		List<String> pts = new ArrayList<String>(); 
+		pts.add("106 23 38 W");
+		pts.add("37 20 19 N");
+		pts.add("9.3");
+		pts.add("9.3");
+		pts.add("106 20 00 W");
+		Assert.assertEquals(
+				"Units [ FEET ], Source DEM [ DTED0 ], Points [ 106 23 38 W, 37 20 19 N, 9.3, 9.3, 106 20 00 W ]",
+				ConversionUtils.toString(pts, "FEET", "DTED0"));
+	}
+}
---
+++ src/test/java/mil/nga/elevation/utils/TestCoordinateUtils.java
@@ -0,0 +1,38 @@
+package mil.nga.elevation.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestCoordinateUtils {
+
+	@Test
+	public void testTruncate() {
+		Assert.assertEquals(1, CoordinateUtils.truncate(1.5));
+		Assert.assertEquals(-100, CoordinateUtils.truncate(-99.5));
+		Assert.assertEquals(99, CoordinateUtils.truncate(99.7));
+		Assert.assertEquals(0, CoordinateUtils.truncate(0.999));
+		Assert.assertEquals(-1, CoordinateUtils.truncate(-0.999));
+	}
+	
+	@Test
+	public void testConvertLat() {
+		Assert.assertEquals("n09",  CoordinateUtils.convertLat(9.5));
+		Assert.assertEquals("n38",  CoordinateUtils.convertLat(38.1));
+		Assert.assertEquals("n37",  CoordinateUtils.convertLat(37.7));
+		Assert.assertEquals("s39",  CoordinateUtils.convertLat(-38.1));
+		Assert.assertEquals("s38",  CoordinateUtils.convertLat(-37.7));
+		Assert.assertEquals("s01",  CoordinateUtils.convertLat(-0.5));
+	}
+	
+	@Test
+	public void testConvertLon() {
+		Assert.assertEquals("e009",  CoordinateUtils.convertLon(9.5));
+		Assert.assertEquals("e038",  CoordinateUtils.convertLon(38.1));
+		Assert.assertEquals("e037",  CoordinateUtils.convertLon(37.7));
+		Assert.assertEquals("e117",  CoordinateUtils.convertLon(117.7));
+		Assert.assertEquals("w039",  CoordinateUtils.convertLon(-38.1));
+		Assert.assertEquals("w038",  CoordinateUtils.convertLon(-37.7));
+		Assert.assertEquals("w001",  CoordinateUtils.convertLon(-0.5));
+		Assert.assertEquals("w117",  CoordinateUtils.convertLon(-116.7));
+	}
+}
